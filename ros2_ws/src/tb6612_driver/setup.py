61e2ed83442859851af074b42301e28ede1ca501
Rishabh Mahajani <rmahajani31@gmail.com>
2025-09-14 13:39:28 -0400

    adding the input file in setup for the motor driver

--- ros2_ws/src/tb6612_driver/setup.py
+++ ros2_ws/src/tb6612_driver/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'tb6612_driver_node = tb6612_driver.tb6612_driver_node:main'
         ],
     },
 )
