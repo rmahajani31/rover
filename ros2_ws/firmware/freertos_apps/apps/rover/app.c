1ef1ebde6abb588ba8864b05f217a644202d21bc
Rishabh Mahajani <rmahajani31@gmail.com>
2026-03-23 08:59:18 -0400

    adding the updated motor control code for 4 motors

--- ros2_ws/firmware/freertos_apps/apps/rover/app.c
+++ ros2_ws/firmware/freertos_apps/apps/rover/app.c
@@ -1,3 +1,4 @@
+// Imports
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
@@ -25,24 +26,31 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
-// Pin Definitions for TB6612
-#define M_LEFT_PWM   18
-#define M_LEFT_IN1   16
-#define M_LEFT_IN2   4
+// LEFT FRONT
+#define M_LF_PWM 18
+#define M_LF_DIR 16
+#define M_LF_SLP 4
+// LEFT REAR
+#define M_LR_PWM 21
+#define M_LR_DIR 22
+#define M_LR_SLP 23
 
-#define M_RIGHT_PWM  19
-#define M_RIGHT_IN1  5
-#define M_RIGHT_IN2  17
-
-#define STBY_PIN     2  // Standby pin must be HIGH for the driver to work
+// RIGHT FRONT
+#define M_RF_PWM 19
+#define M_RF_DIR 5
+#define M_RF_SLP 17
+// RIGHT REAR
+#define M_RR_PWM 25
+#define M_RR_DIR 26
+#define M_RR_SLP 27
 
 #define TRACK_WIDTH 0.5 // meters
 #define STRING_BUFFER_LEN 100
 #define TIMEOUT_MS 500 // Stop motors if no msg for 0.5 seconds
-#define PWM_FREQUENCY 400 // Hz
-#define MIN_PWM 100 // Minimum PWM duty for the motors
-#define MAX_PWM 150 // Maximum PWM duty for the motors
+#define PWM_FREQUENCY 20000 // Hz
+#define DEADZONE 0.05 // m/s
 
+// Global variables
 int64_t last_cmd_vel_time = 0;
 
 rcl_subscription_t cmd_vel_subscriber;
@@ -53,16 +61,20 @@ std_msgs__msg__String outcoming_debug;
 
 char debug_buffer[STRING_BUFFER_LEN]; // Buffer for string messages
 
+// Hardware initialization
 void init_hardware() 
 {
-    // Configure Direction and Standby Pins
-    gpio_reset_pin(M_LEFT_IN1);  gpio_set_direction(M_LEFT_IN1, GPIO_MODE_OUTPUT);
-    gpio_reset_pin(M_LEFT_IN2);  gpio_set_direction(M_LEFT_IN2, GPIO_MODE_OUTPUT);
-    gpio_reset_pin(M_RIGHT_IN1); gpio_set_direction(M_RIGHT_IN1, GPIO_MODE_OUTPUT);
-    gpio_reset_pin(M_RIGHT_IN2); gpio_set_direction(M_RIGHT_IN2, GPIO_MODE_OUTPUT);
-    gpio_reset_pin(STBY_PIN);    gpio_set_direction(STBY_PIN, GPIO_MODE_OUTPUT);
-    
-    gpio_set_level(STBY_PIN, 1); // Enable driver
+	// Configure direction and power pins as gpio outputs
+    uint64_t pin_mask = (1ULL << M_LF_DIR) | (1ULL << M_LF_SLP) |
+                    (1ULL << M_LR_DIR) | (1ULL << M_LR_SLP) |
+                    (1ULL << M_RF_DIR) | (1ULL << M_RF_SLP) |
+                    (1ULL << M_RR_DIR) | (1ULL << M_RR_SLP);
+	gpio_config_t io_conf = {.mode = GPIO_MODE_OUTPUT, .pin_bit_mask = pin_mask};
+	gpio_config(&io_conf);
+
+	// Enable the drivers
+	gpio_set_level(M_LF_SLP, 1); gpio_set_level(M_LR_SLP, 1);
+	gpio_set_level(M_RF_SLP, 1); gpio_set_level(M_RR_SLP, 1);
 
     // Configure PWM (LEDC)
     ledc_timer_config_t timer_conf = {
@@ -74,13 +86,17 @@ void init_hardware()
     };
     ledc_timer_config(&timer_conf);
 
-    ledc_channel_config_t lcd_ch[2] = {
-        {.gpio_num = M_LEFT_PWM,  .channel = LEDC_CHANNEL_0, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0},
-        {.gpio_num = M_RIGHT_PWM, .channel = LEDC_CHANNEL_1, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0}
+    // Configure 4 PWM Channels
+    ledc_channel_config_t lcd_ch[4] = {
+        {.gpio_num = M_LF_PWM, .channel = LEDC_CHANNEL_0, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0},
+        {.gpio_num = M_LR_PWM, .channel = LEDC_CHANNEL_1, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0},
+        {.gpio_num = M_RF_PWM, .channel = LEDC_CHANNEL_2, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0},
+        {.gpio_num = M_RR_PWM, .channel = LEDC_CHANNEL_3, .speed_mode = LEDC_LOW_SPEED_MODE, .timer_sel = LEDC_TIMER_0, .duty = 0}
     };
-    for(int i=0; i<2; i++) ledc_channel_config(&lcd_ch[i]);
+    for(int i=0; i<4; i++) ledc_channel_config(&lcd_ch[i]);
 }
 
+// Initialize Publishers
 void init_publishers(rcl_node_t *node)
 {	
 	// Initialize the debug message
@@ -92,57 +108,80 @@ void init_publishers(rcl_node_t *node)
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "/rover/debug");
 }
 
+// Initialize Subscribers
 void init_subscribers(rcl_node_t *node)
 {
 	RCCHECK(rclc_subscription_init_best_effort(&cmd_vel_subscriber, node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, TwistStamped), "/cmd_vel"));
 }
 
-void set_motor_speed(int channel, int in1_pin, int in2_pin, float speed)
-{
-	float abs_speed = fabsf(speed);
-	uint32_t duty = 0;
-
-	if (abs_speed < 0.01) {
-		duty = 0;
-		gpio_set_level(in1_pin, 0); gpio_set_level(in2_pin, 0);
-	} else {
-		duty = MIN_PWM + (uint32_t)(abs_speed * (255 - MIN_PWM));
-		if (duty > MAX_PWM) duty = MAX_PWM;
-
-		if (speed > 0) {
-            gpio_set_level(in1_pin, 1); gpio_set_level(in2_pin, 0);
-        } else {
-            gpio_set_level(in1_pin, 0); gpio_set_level(in2_pin, 1);
-        }
-	}
-
-	ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, duty);
-	ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+// Helper function to publish debug messages
+void publish_debug(const char * msg) {
+    int len = snprintf(debug_buffer, STRING_BUFFER_LEN, "%s", msg);
+    if (len >= 0 && len < STRING_BUFFER_LEN) {
+        outcoming_debug.data.size = len;
+    } else {
+        outcoming_debug.data.size = STRING_BUFFER_LEN - 1;
+    }
+    rcl_publish(&debug_publisher, (const void*)&outcoming_debug, NULL);
 }
 
+// Helper function to set the motor speed
+void set_motor_speed(int channel, int dir_pin, float speed)
+{
+	// Get magnitude of the speed
+	float abs_speed = fabsf(speed);
+
+	// If the speed is less than the deadzone, set the duty cycle to 0
+	if (abs_speed < DEADZONE) {
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, 0);
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+        return;
+    }
+
+	// Set duty cycle
+	uint32_t duty = (uint32_t)(abs_speed * 255.0f);
+	if (duty > 255) duty = 255;
+
+	// Set direction
+	gpio_set_level(dir_pin, (speed >= 0) ? 1 : 0);
+
+	// Update the PWM hardware channel
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, duty);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+}
+
+// Callback function to convert twist message to motor speeds
 void cmd_vel_subscription_callback (const void * msgin)
 {
-	last_cmd_vel_time = esp_timer_get_time() / 1000; // Refresh the last command velocity time
+	// Refresh the last command velocity time
+	last_cmd_vel_time = esp_timer_get_time() / 1000;
+	
+	// Extract the linear and angular velocities from the incoming message
 	const geometry_msgs__msg__TwistStamped * msg = (const geometry_msgs__msg__TwistStamped *)msgin;
-
 	float linear_x = msg->twist.linear.x;
 	float angular_z = msg->twist.angular.z;
 
-	sprintf(outcoming_debug.data.data, "CMD_VEL_RECEIVED %f %f", linear_x, angular_z);
-	outcoming_debug.data.size = strlen(outcoming_debug.data.data);
-	rcl_publish(&debug_publisher, (const void*)&outcoming_debug, NULL);
+	// Publish the received command velocity
+	char temp_buf[STRING_BUFFER_LEN];
+    snprintf(temp_buf, STRING_BUFFER_LEN, "CMD: %.2f, %.2f", linear_x, angular_z);
+    publish_debug(temp_buf);
 
 	// Kinematic model for differential drive robot
-	float left_velocity = linear_x - (angular_z * TRACK_WIDTH / 2);
-	float right_velocity = linear_x + (angular_z * TRACK_WIDTH / 2);
+	float left_velocity = linear_x - (angular_z * TRACK_WIDTH / 2.0);
+	float right_velocity = linear_x + (angular_z * TRACK_WIDTH / 2.0);
 
-	set_motor_speed(LEDC_CHANNEL_0, M_LEFT_IN1, M_LEFT_IN2, left_velocity);
-	set_motor_speed(LEDC_CHANNEL_1, M_RIGHT_IN1, M_RIGHT_IN2, right_velocity);
+	// Set motor speed for LEFT SIDE MOTORS
+	set_motor_speed(LEDC_CHANNEL_0, M_LF_DIR, left_velocity);
+	set_motor_speed(LEDC_CHANNEL_1, M_LR_DIR, left_velocity);
 
-	sprintf(outcoming_debug.data.data, "LEFT_VELOCITY %f RIGHT_VELOCITY %f", left_velocity, right_velocity);
-	outcoming_debug.data.size = strlen(outcoming_debug.data.data);
-	rcl_publish(&debug_publisher, (const void*)&outcoming_debug, NULL);
+	// Set motor speed for RIGHT SIDE MOTORS
+	set_motor_speed(LEDC_CHANNEL_2, M_RF_DIR, right_velocity);
+	set_motor_speed(LEDC_CHANNEL_3, M_RR_DIR, right_velocity);
+
+	// Publish the left and right motor velocities
+	snprintf(temp_buf, STRING_BUFFER_LEN, "L: %.2f, R: %.2f", left_velocity, right_velocity);
+    publish_debug(temp_buf);
 }
 
 
@@ -189,25 +228,29 @@ void appMain(void *argument)
 		&cmd_vel_subscription_callback, ON_NEW_DATA));
 
 	// Start the executor
+	bool in_safety_stop = false;
+	last_cmd_vel_time = esp_timer_get_time() / 1000;
+	
 	while(1) {
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
 
 		// --- SAFETY STOP LOGIC ---
 		int64_t now = esp_timer_get_time() / 1000;
 		if(now - last_cmd_vel_time > TIMEOUT_MS) {
-			set_motor_speed(LEDC_CHANNEL_0, M_LEFT_IN1, M_LEFT_IN2, 0);
-			set_motor_speed(LEDC_CHANNEL_1, M_RIGHT_IN1, M_RIGHT_IN2, 0);
+			set_motor_speed(LEDC_CHANNEL_0, M_LF_DIR, 0);
+            set_motor_speed(LEDC_CHANNEL_1, M_LR_DIR, 0);
+            set_motor_speed(LEDC_CHANNEL_2, M_RF_DIR, 0);
+            set_motor_speed(LEDC_CHANNEL_3, M_RR_DIR, 0);
 
-			sprintf(outcoming_debug.data.data, "SAFETY STOP - NO CMD_VEL RECEIVED FOR %dms", TIMEOUT_MS);
-			outcoming_debug.data.size = strlen(outcoming_debug.data.data);
-			rcl_publish(&debug_publisher, (const void*)&outcoming_debug, NULL);
+			if (!in_safety_stop) {
+                char temp_buf[STRING_BUFFER_LEN];
+                snprintf(temp_buf, STRING_BUFFER_LEN, "SAFETY STOP - NO CMD_VEL FOR %dms", TIMEOUT_MS);
+                publish_debug(temp_buf);
+                in_safety_stop = true;
+            }
+		} else {
+			in_safety_stop = false;
 		}
-		
-		usleep(10000);
+		vTaskDelay(pdMS_TO_TICKS(10));
 	}
-
-	// Free resources
-	RCCHECK(rcl_publisher_fini(&debug_publisher, &node));
-	RCCHECK(rcl_subscription_fini(&cmd_vel_subscriber, &node));
-	RCCHECK(rcl_node_fini(&node));
 }
