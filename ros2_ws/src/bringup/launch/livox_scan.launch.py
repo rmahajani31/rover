c77a18dd2e0839e43e9a0de79309adddac195b0f
Rishabh Mahajani <rmahajani31@gmail.com>
2026-05-25 00:58:41 +0800

    adding bringup file for livox scan to 2d laser scan

---
+++ ros2_ws/src/bringup/launch/livox_scan.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    livox_cloud_to_scan_launch = PathJoinSubstitution([
+        FindPackageShare("livox_cloud_to_scan"),
+        "livox_cloud_to_scan.launch.py",
+    ])
+
+    return LaunchDescription([
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="odom_to_base_link_tf",
+            arguments=[
+                "0", "0", "0",
+                "0", "0", "0",
+                "odom", "base_link",
+            ],
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="base_link_to_livox_tf",
+            arguments=[
+                "0.0", "0.0", "0.3175",
+                "0", "0", "0",
+                "base_link", "livox_frame",
+            ],
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(livox_cloud_to_scan_launch)
+        ),
+    ])
--- ros2_ws/src/bringup/setup.py
+++ ros2_ws/src/bringup/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/rover.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/rover.launch.py', 'launch/livox_scan.launch.py']),
         ('share/' + package_name + '/urdf', ['urdf/rover.urdf']),
         ('share/' + package_name + '/config', ['config/slam_toolbox_async.yaml']),
     ],
