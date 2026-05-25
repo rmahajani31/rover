fc21ae6672ff11de70d8b57c6074c15c0cfa0abc
Rishabh Mahajani <rmahajani31@gmail.com>
2026-05-24 20:17:12 -0400

    adding launch file and config for building rover_odometry

---
+++ ros2_ws/src/rover_odometry/launch/rover_odometry.launch.py
@@ -0,0 +1,20 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory("rover_odometry")
+    config_file = os.path.join(pkg_dir, "config", "rover_odometry.yaml")
+
+    return LaunchDescription([
+        Node(
+            package="rover_odometry",
+            executable="odometry_node",
+            name="odometry",
+            output="screen",
+            parameters=[config_file],
+        ),
+    ])
