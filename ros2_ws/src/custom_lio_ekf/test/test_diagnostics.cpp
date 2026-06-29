d650e272debaa399a48423c000f0b5f89420085e
Rishabh Mahajani <rmahajani31@gmail.com>
2026-06-28 20:43:08 -0400

    fixing diagnostics cmake issue

---
+++ ros2_ws/src/custom_lio_ekf/test/test_diagnostics.cpp
@@ -0,0 +1,56 @@
+#include "custom_lio_ekf/diagnostics.hpp"
+
+#include <gtest/gtest.h>
+
+#include <diagnostic_msgs/msg/diagnostic_status.hpp>
+
+namespace custom_lio_ekf
+{
+namespace
+{
+
+TEST(LioEkfDiagnostics, DefaultDiagnosticsMessageIsPublishable)
+{
+  LioEkfDiagnostics diagnostics;
+  diagnostics.input_points = 42;
+  diagnostics.map_points = 0;
+
+  builtin_interfaces::msg::Time stamp;
+  stamp.sec = 12;
+  stamp.nanosec = 34;
+
+  const auto message = makeDiagnosticArray(diagnostics, stamp, "custom_lio_ekf");
+
+  ASSERT_EQ(message.status.size(), 1U);
+  EXPECT_EQ(message.header.stamp.sec, stamp.sec);
+  EXPECT_EQ(message.header.stamp.nanosec, stamp.nanosec);
+  EXPECT_EQ(message.status.front().name, "custom_lio_ekf");
+  EXPECT_EQ(message.status.front().hardware_id, "custom_lio_ekf");
+  EXPECT_EQ(
+    message.status.front().level,
+    diagnostic_msgs::msg::DiagnosticStatus::WARN);
+  EXPECT_EQ(message.status.front().message, "local_map_not_initialized");
+  EXPECT_FALSE(message.status.front().values.empty());
+}
+
+TEST(LioEkfDiagnostics, SuccessfulUpdateReportsOk)
+{
+  LioEkfDiagnostics diagnostics;
+  diagnostics.map_initialized = true;
+  diagnostics.prediction.success = true;
+  diagnostics.prediction.status = "success";
+  diagnostics.lidar_update.success = true;
+  diagnostics.lidar_update.status = "success";
+
+  const auto message =
+    makeDiagnosticArray(diagnostics, builtin_interfaces::msg::Time{}, "custom_lio_ekf");
+
+  ASSERT_EQ(message.status.size(), 1U);
+  EXPECT_EQ(
+    message.status.front().level,
+    diagnostic_msgs::msg::DiagnosticStatus::OK);
+  EXPECT_EQ(message.status.front().message, "success");
+}
+
+}  // namespace
+}  // namespace custom_lio_ekf
