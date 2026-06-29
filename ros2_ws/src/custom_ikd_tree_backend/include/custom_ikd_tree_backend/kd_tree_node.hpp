317e5979275f7bf20bbf5498daf6cea47f806645
Rishabh Mahajani <rmahajani31@gmail.com>
2026-06-29 07:59:11 -0400

    adding the main node struct for the ikd tree

---
+++ ros2_ws/src/custom_ikd_tree_backend/include/custom_ikd_tree_backend/kd_tree_node.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+
+#include <Eigen/Core>
+
+#include "custom_ikd_tree_backend/bounding_box.hpp"
+
+namespace custom_ikd_tree_backend
+{
+
+struct KdTreeNode
+{
+  explicit KdTreeNode(
+    const Eigen::Vector3d& point_in = Eigen::Vector3d::Zero(),
+    int split_axis_in = 0);
+
+  Eigen::Vector3d point = Eigen::Vector3d::Zero();
+
+  std::unique_ptr<KdTreeNode> left;
+  std::unique_ptr<KdTreeNode> right;
+
+  int split_axis = 0;
+  int tree_size = 1;
+
+  BoundingBox box;
+
+  bool deleted = false;
+  bool subtree_deleted = false;
+  int invalid_count = 0;
+};
+
+int sizeOf(const std::unique_ptr<KdTreeNode>& node);
+
+int invalidCountOf(const std::unique_ptr<KdTreeNode>& node);
+
+bool isActive(const KdTreeNode& node);
+
+void updateMetadata(KdTreeNode& node);
+
+}  // namespace custom_ikd_tree_backend
